667ea3a9dac73c297319e2d90b1374a782e6cd03
ammara312 <sazkam@hotmail.com>
2017-10-24 23:50:41 +0200

    Introducing Admin role and moderating comment and product section

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,8 +5,11 @@ class Ability
     #:manage... any user can manage (perform any action) with a User object. 
     #user must have the same ID as his or her user record does (i.e., users can manage themselves).
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
-
+    if user.admin?
+      can :manage, :all
+    else
+      can :manage, User, id: user.id #otherwise user can manage user where id = user.id (self manage)
+    end
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
   end
