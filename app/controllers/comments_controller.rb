978e1ebe1e471aadc296f9e3ccde364415845e94
ammara312 <sazkam@hotmail.com>
2017-11-17 17:12:05 +0100

    ex 6.14 active job and advanced actioncable implemented

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,10 +5,14 @@ class CommentsController < ApplicationController
     @comment = @product.comments.new(comment_params)
     @comment.user = current_user
     @comment.save
+    @user = current_user
     respond_to do |format|
       # (byebug)
       if @comment.save
-        ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
+        #ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
+        #for broadcasting
+        #ProductChannel.broadcast_to @product.id, comment: CommentsController.render(partial: 'comments/comment', 
+        #  locals: {comment: @comment, current_user: current_user}), average_rating: @product.average_rating
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
         format.json { render :show, status: :created, location: @product }
         format.js
